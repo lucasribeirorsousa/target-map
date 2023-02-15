325adbc4871c929de3526992c5b9e99d8f162964
Lucas Ribeiro <lucasguitar45@gmail.com>
2023-02-15 03:58:54 -0300

    Add crud views

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from apps.targets.views import home, add_target
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    path('add_target/', add_target, name='add_target'),
+    path('', include('apps.targets.urls')),
 ]
