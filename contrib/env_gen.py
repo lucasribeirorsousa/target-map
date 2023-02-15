397a3135763c23a0c92a7f702da9ee45e869610a
Lucas Ribeiro <lucasguitar45@gmail.com>
2023-02-15 15:02:30 -0300

    Add user for test

---
+++ contrib/env_gen.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+"""
+Django SECRET_KEY generator.
+"""
+from django.utils.crypto import get_random_string
+
+
+chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+
+CONFIG_STRING = """
+SECRET_KEY=%s
+DB_NAME=
+DB_USER=
+DB_PASSWORD=
+DB_HOST=
+DB_PORT=
+""".strip() % get_random_string(50, chars)
+
+# Writing our configuration file to '.env'
+with open('.env', 'w') as configfile:
+    configfile.write(CONFIG_STRING)
