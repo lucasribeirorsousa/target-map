c4aad1c9258f6c3d720b8bc448127da14e6cef29
Lucas Ribeiro <lucasguitar45@gmail.com>
2023-02-15 09:04:22 -0300

    Update url home

--- apps/targets/urls.py
+++ apps/targets/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import home, add_target
 
 urlpatterns = [
-    path('home/', home, name='home'),
+    path('', home, name='home'),
     path('add_target/', add_target, name='add_target'),
 ]
